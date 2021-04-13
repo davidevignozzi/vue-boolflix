d3c1f6ad299dd16522a58671ca11f4a40aae2bb5
Davide Vignozzi <davide.vignozzi@gmail.com>
2021-04-13 16:46:40 +0200

    flag languages (Country Flags API)

--- js/script.js
+++ js/script.js
@@ -1,47 +1,48 @@
 var app = new Vue({
 
-    el : '#app',
+  el : '#app',
 
-    data: {
-        filmList: [],
-        tvSeriesList: [],
-        inSearch: "",
-    },
+  data: {
+    filmList: [],
+    tvSeriesList: [],
+    inSearch: "",
+  },
     
-    methods: {
+  methods: {
 
-        // to search a film
-        filmSearch: function() {
-            axios.get('https://api.themoviedb.org/3/search/movie', {
-              params: {
-                api_key: "11be422449662a8ede36c26b4599a9c8",
-                query: this.inSearch,
-                language: "it-IT",
-              }
-            })
-
-            .then((request)=>{
-                const result = request.data.results;
-                this.filmList = result;
-                this.starVote();
-                console.log(result)
-            })
-            this.inSearch = "";
-        },
-        
-        // to search a tv serie
-
-        // star
-        starVote: function() {
-            this.filmList.forEach((film) => {
-              const voteRound = (film.vote_average / 2);
-              let vote = Math.ceil(voteRound);
-              film.vote_average = vote;
-              console.log(film.vote_average);
-            });
+    // to search a film
+    filmSearch: function() {
+      axios.get('https://api.themoviedb.org/3/search/movie', {
+        params: {
+          api_key: "11be422449662a8ede36c26b4599a9c8",
+          query: this.inSearch,
+          language: "it-IT",
+          include_adult: false,
         }
+      })
 
+      .then((request)=>{
+        const result = request.data.results;
+        this.filmList = result;
+        this.starVote();
+        console.log(result)
+      })
+      this.inSearch = "";
     },
+        
+    // to search a tv serie
+
+
+    // star
+    starVote: function() {
+      this.filmList.forEach((film) => {
+        const voteRound = (film.vote_average / 2);
+        let vote = Math.ceil(voteRound);
+        film.vote_average = vote;
+      });
+    }
+
+  },
 
 
 })
