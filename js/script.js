6576e485ce21e8b66cbabe6609888102efe280e3
Davide Vignozzi <davide.vignozzi@gmail.com>
2021-04-13 17:25:57 +0200

    add tv series in the script

--- js/script.js
+++ js/script.js
@@ -10,13 +10,13 @@ var app = new Vue({
     
   methods: {
 
-    // to search a film
     filmSearch: function() {
+
+      // to search a film
       axios.get('https://api.themoviedb.org/3/search/movie', {
         params: {
           api_key: "11be422449662a8ede36c26b4599a9c8",
           query: this.inSearch,
-          language: "it-IT",
           include_adult: false,
         }
       })
@@ -27,13 +27,24 @@ var app = new Vue({
         this.starVote();
         console.log(result)
       })
-      this.inSearch = "";
+
+      // to search a tv serie
+      axios.get('https://api.themoviedb.org/3/search/tv', {
+        params: {
+          api_key: "11be422449662a8ede36c26b4599a9c8",
+          query: this.inSearch,
+        }
+      })
+
+      .then((request) => {
+        this.tvSeriesList = request.data.results;
+        console.log(this.tvSeriesList);
+      });
+      // to empty the input
+      this.inSearch="";
     },
         
-    // to search a tv serie
-
-
-    // star
+    // star vote
     starVote: function() {
       this.filmList.forEach((film) => {
         const voteRound = (film.vote_average / 2);
